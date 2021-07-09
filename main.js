bfc2ca64606680258cc413f204b7d35f612ba2d3
kmgb <25809479+kmgb@users.noreply.github.com>
2021-07-08 20:17:50 -0400

    Move TOKEN to env, refactor

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,76 +8,78 @@ client.once('ready', () => {
     console.log('Discord.js ready');
 });
 
-client.login('get your own token, freeloader :)');
+if (process.env.TOKEN) {
+    client.login(process.env.TOKEN);
+}
+else {
+    console.log('TOKEN needs to be provided in environment variable');
+    process.exit(1);
+}
 
 // There are other emoji that may be used to substitute letters,
 // this could allow us to display multiple of the same letter in the future
-const alphabetMap = {
-    'a': '🇦',
-    'b': '🇧',
-    'c': '🇨',
-    'd': '🇩',
-    'e': '🇪',
-    'f': '🇫',
-    'g': '🇬',
-    'h': '🇭',
-    'i': '🇮',
-    'j': '🇯',
-    'k': '🇰',
-    'l': '🇱',
-    'm': '🇲',
-    'n': '🇳',
-    'o': '🇴',
-    'p': '🇵',
-    'q': '🇶',
-    'r': '🇷',
-    's': '🇸',
-    't': '🇹',
-    'u': '🇺',
-    'v': '🇻',
-    'w': '🇼',
-    'x': '🇽',
-    'y': '🇾',
-    'z': '🇿',
-};
+const alphabetMap = new Map([
+    ['a', '🇦'],
+    ['b', '🇧'],
+    ['c', '🇨'],
+    ['d', '🇩'],
+    ['e', '🇪'],
+    ['f', '🇫'],
+    ['g', '🇬'],
+    ['h', '🇭'],
+    ['i', '🇮'],
+    ['j', '🇯'],
+    ['k', '🇰'],
+    ['l', '🇱'],
+    ['m', '🇲'],
+    ['n', '🇳'],
+    ['o', '🇴'],
+    ['p', '🇵'],
+    ['q', '🇶'],
+    ['r', '🇷'],
+    ['s', '🇸'],
+    ['t', '🇹'],
+    ['u', '🇺'],
+    ['v', '🇻'],
+    ['w', '🇼'],
+    ['x', '🇽'],
+    ['y', '🇾'],
+    ['z', '🇿'],
+]);
 
-client.on('message', message => {
-	if (message.content === 'apple') {
-        message.react('🍎');
-        return;
-    }
+client.on('message', async (message) => {
+    var reactions = tryConvertToReactions(message.content);
 
-    // TODO: Restructuring needed
-    var isValid = isStringValidForReact(message.content);
-    var reactions = [];
-    if (isValid) {
-        var text = message.content.toLowerCase(); // TODO: English only for now :(
-        var letterCount = count(text);
-        for (const elem of letterCount) {
-            if (elem[1] > 1) {
-                isValid = false;
-            }
-        }
-
-        if (isValid) {
-            for (var i = 0; i < text.length; i++) {
-                reactions.push(alphabetMap[text.charAt(i)]);
-            }
-        }
-    }
-
-    if (isValid) {
+    if (reactions) {
         for (const r of reactions) {
-            // ! Hacky way of getting around await-only-in-async-functions requirements
-            (async function() {
-                await message.react(r);
-            })();
+            await message.react(r);
         }
     }
     else {
-        message.react('❌');
+        await message.react('❌');
     }
 });
+
+function tryConvertToReactions(inText) {
+    var text = inText.toLowerCase(); // TODO: English only for now :(
+
+    // Check text to ensure:
+    // 1. Each letter is in our alphabet
+    // 2. Each letter only shows up once
+    var letterCount = count(text);
+    for (const elem of letterCount) {
+        if (!alphabetMap.has(elem[0]) || elem[1] > 1) {
+            return null;
+        }
+    }
+
+    var reactions = [];
+    for (var i = 0; i < text.length; i++) {
+        reactions.push(alphabetMap.get(text.charAt(i)));
+    }
+
+    return reactions;
+}
 
 // TODO: Not sure how safe this would be with non-English
 function count(string) {
@@ -85,9 +88,4 @@ function count(string) {
        count.get(s) ? count.set(s, count.get(s) + 1) : count.set(s, 1);
     });
     return count;
-}
-
-function isStringValidForReact(s) {
-    const english = /^[A-Za-z0-9]*$/;
-    return english.test(s);
 }
